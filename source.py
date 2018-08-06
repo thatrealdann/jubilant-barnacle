5f7a5fde18fb1069047471d4923b789c95f2afd1
Daniel Mir <thatrealdann@gmail.com>
2018-08-06 09:37:32 +0430

    Initial commit

---
+++ source.py
@@ -0,0 +1,130 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+
+if not os.path.exists('images'):
+    os.mkdir('images')
+
+
+img_rows = 28
+img_cols = 28
+channels = 1
+img_shape = (img_rows, img_cols, channels)
+latent_dim = 100
+epochs = 5000
+batch_size = 128
+sample_interval = 50
+colab = False
+
+
+(x_train, _), (_, _) = mnist.load_data()
+
+x_train = x_train / 127.5 - 1
+x_train = np.expand_dims(x_train, axis=3)
+
+valid = np.ones((batch_size, 1))
+fake = np.zeros((batch_size, 1))
+
+
+def build_generator():
+    model = Sequential()
+    model.add(Dense(units=256, input_dim=latent_dim))
+    model.add(LeakyReLU(alpha=0.5))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(units=512))
+    model.add(LeakyReLU(alpha=0.5))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(units=1024))
+    model.add(LeakyReLU(alpha=0.5))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(units=28 * 28, activation='tanh'))
+    model.add(Reshape(target_shape=img_shape))
+
+    model.summary()
+
+    noise = Input(shape=(latent_dim,))
+    img = model(noise)
+
+    return Model(noise, img)
+
+
+def build_discriminator():
+    model = Sequential()
+    model.add(Flatten(input_shape=img_shape))
+    model.add(Dense(units=512))
+    model.add(LeakyReLU(alpha=0.5))
+    model.add(Dense(units=256))
+    model.add(LeakyReLU(alpha=0.5))
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    model.summary()
+    img = Input(shape=img_shape)
+    validity = model(img)
+
+    return Model(inputs=img, outputs=validity)
+
+
+optimizer = Adam(0.0002, 0.5)
+
+discriminator = build_discriminator()
+discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+generator = build_generator()
+
+z = Input(shape=(latent_dim,))
+img = generator(z)
+
+discriminator.trainable = False
+validity = discriminator(img)
+
+combined = Model(z, validity)
+combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+
+def sample_images(epoch):
+    r, c = 5, 5
+    noise = np.random.normal(0, 1, (r * c, latent_dim))
+    gen_imgs = generator.predict(noise)
+
+    gen_imgs = 0.5 * gen_imgs + 0.5
+
+    fig, axs = plt.subplots(r, c)
+    cnt = 0
+    for i in range(r):
+        for j in range(c):
+            axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+            axs[i,j].axis('off')
+            cnt += 1
+
+    if not colab:
+        fig.savefig("images/%d.png" % epoch)
+    else:
+        plt.show()
+    plt.close()
+
+
+for epoch in range(epochs):
+        idx = np.random.randint(0, x_train.shape[0], batch_size)
+        imgs = x_train[idx]
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        gen_imgs = generator.predict(noise)
+
+        d_loss_real = discriminator.train_on_batch(imgs, valid)
+        d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
+
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        g_loss = combined.train_on_batch(noise, valid)
+
+        print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
+
+        if epoch % sample_interval == 0:
+                sample_images(epoch)
+
